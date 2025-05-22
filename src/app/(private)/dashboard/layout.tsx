0e8804e159bdcfa2fdf25ff105e3bf3d40916a09
diegorezm <dmourarezende@email.com>
2025-05-22 15:41:32 -0300

    adding react query provider

--- src/app/(private)/dashboard/layout.tsx
+++ src/app/(private)/dashboard/layout.tsx
@@ -1,10 +1,11 @@
+import { QueryProvider } from "@/providers/query-provider";
 import MobileNavigation from "./_components/MobileNavigation";
 import Sidebar from "./_components/Sidebar";
 import styles from "./dashboard.module.css"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <QueryProvider>
       <div className={styles.mobileNav}>
         <MobileNavigation />
       </div>
@@ -14,6 +15,6 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <main className={styles.main}>
         {children}
       </main>
-    </>
+    </QueryProvider>
   )
 }
--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function DashboardPage() {
-  return <div>testing Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore praesentium, ducimus accusantium, voluptas architecto sapiente corporis nulla vitae dicta obcaecati, explicabo sint. Sapiente neque sequi debitis enim fuga. Aut, nulla.</div>;
+  return redirect("/dashboard/churches")
 }
